3c4d1d9ff286c95dbd1e46fb394d5b4c7e565951
aidenwliu <75360211+aidenwliu@users.noreply.github.com>
2021-01-12 15:03:36 +0800

    Create 695_max_area_of_island.java

---
+++ Recursion/695_max_area_of_island.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int max = 0;
+        for (int i = 0; i<grid.length;i++) {
+        for (int j = 0; j<grid[0].length;j++) { 
+            if (grid[i][j] == 1)
+                max = Math.max(max,dfs(grid, i, j)); }
+        }
+        return max;
+    }
+public int dfs(int[][] grid, int i, int j) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length ||grid[i][j] != 1)
+    return 0;
+    grid[i][j] = -1;
+    int count = 1;
+    count += dfs(grid, i+1, j); 
+    count += dfs(grid, i-1, j); 
+    count += dfs(grid, i, j+1); 
+    count += dfs(grid, i, j-1); 
+    return count;
+    }
+}
