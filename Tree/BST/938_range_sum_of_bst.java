5e705feb649ed2af68e7121f7c7c4c34e8303c6b
aidenwliu <75360211+aidenwliu@users.noreply.github.com>
2021-01-21 18:29:41 +0800

    Create 938_range_sum_of_bst.java

---
+++ Tree/BST/938_range_sum_of_bst.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int res = 0;
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if (cur.val >= low && cur.val <= high) 
+                res += cur.val;
+            if (cur.left != null && cur.val > low)
+                queue.add(cur.left);
+            if (cur.right != null && cur.val < high)
+                queue.add(cur.right);
+        }
+        return res;
+    }
+}
