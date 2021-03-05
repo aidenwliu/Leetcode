5ffd3e71a20f0501ab663f573d4b118fda860d08
aidenwliu <75360211+aidenwliu@users.noreply.github.com>
2021-03-05 11:41:23 +0800

    Update 46_permutations.java

--- Recursion/46_permutations.java
+++ Recursion/46_permutations.java
@@ -20,3 +20,9 @@ class Solution {
         }
     }
 }
+//Time complexity: O(n!)
+// in the first level of the tree, you have N options,
+// and for each of the option, you have N-1 option, 
+// and for each of these N-1 options, you have another N-2 options, 
+// so putting them together you would end up N*(N-1)*(N-2).... = N!
+//Space complexity: O(n!)
